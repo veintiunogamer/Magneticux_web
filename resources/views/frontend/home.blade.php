fd498d001007321231f09c28e12d30ea1e97db42
Jose Alzate <josealzate97@gmail.com>
2025-06-09 17:55:35 -0500

    Add landing page routes and update home view
    - Created a route for the landing page that returns the 'frontend.home' view.
    - Added a separate route for '/home' that also returns the 'frontend.home' view.
    - Commented out the default Auth routes for future implementation.
    - Organized routes with comments for better clarity on public and backend routes.

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Enfoque 21</title>
+
+        <!-- Styles / Scripts -->
+
+
+
+    </head>
+
+    <body>
+        Home
+    </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+
+/*
+ * ✅ Rutas para landing publica
+*/
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home'); // Pagina Inicio
 });
+
+Route::get('/home', function () {
+    return view('frontend.home'); // Pagina Inicio
+});
+
+
+/*
+ * ✅ Auth (puedes usar Laravel Breeze, Fortify o Jetstream)
+*/
+// Auth::routes();
+
+
+/*
+ * ✅ Rutas para dashboard - backend
+*/
