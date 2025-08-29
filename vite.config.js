55ac9b51ef8a2d940d719f1120c2603250eef9df
Jose Alzate <josealzate97@gmail.com>
2025-08-28 21:26:15 -0500

    Actualizar la configuración del entorno y los archivos de Docker para reflejar el nombre del proyecto 'magneticux'.

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'enfoque21.localhost',
+            host: 'magneticux.localhost',
             protocol: 'ws',
         },
     },
