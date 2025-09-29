69b2327863bd7d608a7dfad12487c8c4afb84e51
Jose Alzate <josealzate97@gmail.com>
2025-09-29 12:17:37 -0500

    Agregar archivo de configuración para la ruta de construcción de Vite en Laravel.

---
+++ config/vite.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Vite Build Path
+    |--------------------------------------------------------------------------
+    |
+    | Esta opción le dice a Laravel dónde encontrar los archivos generados
+    | por Vite en producción.
+    |
+    */
+
+    'build_path' => public_path('build'),
+];
