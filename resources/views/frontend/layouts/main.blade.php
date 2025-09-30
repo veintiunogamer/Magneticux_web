ad68b87565c929d4a0892e2872a41f5aaba9e163
Jose Alzate <josealzate97@gmail.com>
2025-09-30 11:06:27 -0500

    Refactor package.json and main.blade.php
    - Removed unnecessary devDependencies from package.json: @tailwindcss/vite and laravel-vite-plugin.
    - Updated script tag in main.blade.php to remove 'defer' attribute for app.js.

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -7,9 +7,8 @@
         <title>@yield('title', 'Magnetic UX')</title>
 
         <!-- Estilos -->
-        
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}"></script>
 
         <!-- Font Awesome 6.5.2 -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="" crossorigin="anonymous" referrerpolicy="no-referrer" />
